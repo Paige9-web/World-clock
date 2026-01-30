b6d239dbbeaaf3efdc3e977c9539eb667ab97a7c
Paige Warren <Paigewarren1@icloud.com>
2026-01-30 15:07:32 +0000

    updated JS to add real time data

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
 
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  // manila
+  let manilaElement = document.querySelector("#manila");
+  let manilaDateElement = manilaElement.querySelector(".date");
+  let manilaTimeElement = manilaElement.querySelector(".time");
+
+  let manilaTime = moment().tz("Europe/Italy");
+
+  manilaDateElement.innerHTML = manilaTime.format("MMMM Do YYYY");
+  manilaTimeElement.innerHTML = manilaTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+}
+
+setInterval(updateTime, 1000);
+updateTime();
