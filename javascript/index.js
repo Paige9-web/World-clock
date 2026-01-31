26d2e71dd2b428d612871fb372319475e27d4174
Paige Warren <Paigewarren1@icloud.com>
2026-01-31 09:04:39 +0000

    fixed the JS to show dropdown times

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,57 @@
+let timer = null;
 function updateTime() {
-  // Tokyo
-  let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-
   let tokyoTime = moment().tz("Asia/Tokyo");
+  let manilaTime = moment().tz("Asia/Manila");
 
-  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+  document.querySelector("#tokyo .date").innerHTML =
+    tokyoTime.format("MMMM Do YYYY");
 
-  // manila
-  let manilaElement = document.querySelector("#manila");
-  let manilaDateElement = manilaElement.querySelector(".date");
-  let manilaTimeElement = manilaElement.querySelector(".time");
+  document.querySelector("#tokyo .time").innerHTML =
+    `${tokyoTime.format("h:mm:ss")} <small>${tokyoTime.format("A")}</small>`;
 
-  let manilaTime = moment().tz("Europe/Italy");
+  document.querySelector("#manila .date").innerHTML =
+    manilaTime.format("MMMM Do YYYY");
 
-  manilaDateElement.innerHTML = manilaTime.format("MMMM Do YYYY");
-  manilaTimeElement.innerHTML = manilaTime.format(
-    "h:mm:ss [<small>]A[</small>]",
-  );
+  document.querySelector("#manila .time").innerHTML =
+    `${manilaTime.format("h:mm:ss")} <small>${manilaTime.format("A")}</small>`;
 }
 
-setInterval(updateTime, 1000);
+// Dropdown change
+function updateCity(event) {
+  clearInterval(timer);
+
+  let cityTimeZone = event.target.value;
+  if (!cityTimeZone) {
+    timer = setInterval(updateTime, 1000);
+    updateTime();
+    return;
+  }
+
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
+  function updateSelectedCity() {
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = cityTimeZone.split("/")[1].replace("_", " ");
+
+    document.querySelector("#cities").innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">
+        ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
+      </div>
+    </div>
+  `;
+  }
+
+  updateSelectedCity();
+  timer = setInterval(updateSelectedCity, 1000);
+}
+timer = setInterval(updateTime, 1000);
 updateTime();
+
+document.querySelector("#city").addEventListener("change", updateCity);
